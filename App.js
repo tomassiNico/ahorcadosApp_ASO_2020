a3f1fc641e5af9fa66265b252c642120cc101cb3
Tomassi Nicolás <leguizamon.tomassi@gmail.com>
2020-06-01 11:23:28 -0300

    Se cambia stack de navegacion.

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 import { SplashScreen } from 'expo';
 import * as Font from 'expo-font';
@@ -10,17 +10,18 @@ import MenuScreen from './src/Home/screen/MenuScreen';
 import {GameScreenWithContext} from './src/Game/screens/GameScreen';
 import LoginScreen from './src/Login/screens/LoginScreen';
 import useLinking from './navigation/useLinking';
-import {AppProvider} from './src/Shared/providers/appProvider';
+import {AppProvider, store} from './src/Shared/providers/appProvider';
 import Notifications from './src/Home/components/Notifications';
 import Logout from "./src/Home/components/Logout";
 import VersusScreen from './src/Game/screens/VersusScreen';
 const Stack = createStackNavigator();
 
-export default function App(props) {
+const Navigator = (props) => {
   const [isLoadingComplete, setLoadingComplete] = React.useState(false);
   const [initialNavigationState, setInitialNavigationState] = React.useState();
   const containerRef = React.useRef();
   const { getInitialState } = useLinking(containerRef);
+  const globalData = useContext(store);
 
   // Load any resources or data that we need prior to rendering the app
   React.useEffect(() => {
@@ -54,31 +55,43 @@ export default function App(props) {
     return (
           <View style={styles.container}>
             {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-            <AppProvider>
               <NavigationContainer ref={containerRef} initialState={initialNavigationState}>
                 <Stack.Navigator initialRouteName={"MenuScreen"}>
+                {!globalData.state.username ? (
                   <Stack.Screen name="Login" component={LoginScreen} />
+                ) : (
+                  <>
                   <Stack.Screen
                       name="Menu"
                       component={MenuScreen}
                       options={({ navigation }) => ({
                         headerRight: () => (
                             <View>
-                              <Logout navigation={navigation}/>
+                              <Logout/>
                               <Notifications navigation={navigation}/>
                             </View>),
                       })}
                   />
                   <Stack.Screen name="Game" component={GameScreenWithContext} />
                   <Stack.Screen name="VS" component={VersusScreen} />
+                  </>
+                )}
                 </Stack.Navigator>
               </NavigationContainer>
-            </AppProvider>
-
           </View>
     );
   }
 }
+
+export default function App(props) {
+  return (
+    <AppProvider>
+      <Navigator {...props}/>
+    </AppProvider>
+  )
+}
+
+
 
 const styles = StyleSheet.create({
   container: {
