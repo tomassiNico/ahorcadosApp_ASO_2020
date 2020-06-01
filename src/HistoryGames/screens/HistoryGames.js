63dca9621e2001718ea27c905e8e33208296a16c
Alexis Fabián Carranza <AFCarranza@proveedor.teco.com.ar>
2020-06-01 17:20:57 -0300

    * Se actualiza definicion de ganador

--- src/HistoryGames/screens/HistoryGames.js
+++ src/HistoryGames/screens/HistoryGames.js
@@ -1,15 +1,44 @@
-import React, { useContext } from 'react';
-import { Platform, StatusBar, StyleSheet, View, Text } from 'react-native';
+import React, {useContext, useEffect} from 'react';
+import { View } from 'react-native';
 import {store} from '../../Shared/providers/appProvider';
 import { FlatList } from 'react-native-gesture-handler';
 import GameResume from '../components/GameResume';
+import singletonFirebaseServices from "../../Shared/services/firebaseService";
+
+const getWinner = (game) => {
+    const {state1, state2, time1, time2, username1, username2 } =  game;
+    let winner;
+    if(state1 === state2 && time1 === time2){
+        return "empate"
+    }
+
+    if(state2 !== state1){
+        winner = state1 === "win" ? username1 : username2;
+    }else if(state1 === "lose" && state1 === "lose"){
+        winner = "empate";
+    }else {
+        winner = time1 < time2 ? username1 : username2;
+    }
+    return winner
+};
 
 const HistoryGames = () => {
   const { state } = useContext(store);
   const games = state.games ? state.games : [];
+
+  useEffect(()=> {
+      let gamesWihoutWinner = games.filter(game => game.winner === '');
+      gamesWihoutWinner.forEach(game => {
+          if (game.state1 && game.state2) {
+              let winner = getWinner(game);
+              let updatedData = { winner };
+              singletonFirebaseServices.updateData('games', game.idGame, updatedData);
+          }
+      })
+  }, [games])
   return(
     <View>
-      <FlatList 
+      <FlatList
         data={games}
         renderItem={({ item }) => <GameResume game={item} />}
         keyExtractor={(item) => item.idGame}
@@ -17,5 +46,7 @@ const HistoryGames = () => {
     </View>
   )
 }
+
+
 
 export default HistoryGames
